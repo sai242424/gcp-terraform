2599be6f08c052a625086483ab26c067773d957c
Sai <sai.reddy@teamsada.com>
2021-12-06 21:55:04 -0600

    adding clouduild and terraform files

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  region = "us-central1"
+}
+
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+
+  labels = {
+    foo = "bar"
+  }
+
+  message_retention_duration = "86600s"
+}
