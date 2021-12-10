06d3dfd2490b36136c4fa7f28c6d86a6dde28e0f
Sai <sai.reddy@teamsada.com>
2021-12-09 21:38:21 -0600

    updating topic name , table name and table schema in main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ provider "google" {
   region = "us-central1"
 }
 
-/*
-resource "google_pubsub_topic" "example1" {
-  name = "dataplatformfoundation_test_1"
+
+resource "google_pubsub_topic" "stream1" {
+  name = "dataplatformfoundation_stream_json"
 
   message_retention_duration = "86600s"
 }
-*/
+
 
 resource "google_bigquery_table" "default" {
   dataset_id = "dataplatformfoundation_streaming"
-  table_id   = "dataplatform_usa_names_terraform"
+  table_id   = "dataplatform_usa_names_stream_json_terraform"
 
   time_partitioning {
     type = "DAY"
@@ -26,41 +26,95 @@ resource "google_bigquery_table" "default" {
   schema = <<EOF
 [
   {
-    "name": "state",
+    "name": "subverticals",
     "type": "STRING",
     "mode": "NULLABLE",
-    "description": "State"
+    "description": "subverticals"
   },
   {
-    "name": "gender",
+    "name": "job_location",
     "type": "STRING",
     "mode": "NULLABLE",
-    "description": "Gender"
+    "description": "job_location"
   },
   {
-    "name": "year",
+    "name": "job_description_id",
     "type": "INTEGER",
     "mode": "NULLABLE",
-    "description": "Year"
+    "description": "job_description_id"
   },
   {
-    "name": "name",
+    "name": "job_description",
     "type": "STRING",
     "mode": "NULLABLE",
-    "description": "Name"
+    "description": "job_description"
   },
   {
-    "name": "number",
+    "name": "jd_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "jd_id"
+  },
+  {
+    "name": "job_title",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "job_title"
+  },
+  {
+    "name": "ecosystem_company_id",
     "type": "INTEGER",
     "mode": "NULLABLE",
-    "description": "Number"
+    "description": "ecosystem_company_id"
   },
   {
-    "name": "created_date",
+    "name": "job_posting_date",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE",
+    "description": "job_posting_date"
+  },
+  {
+    "name": "core_skills",
     "type": "STRING",
     "mode": "NULLABLE",
-    "description": "Created_date"
-  }  
+    "description": "core_skills"
+  },
+  {
+    "name": "job_title",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "job_title"
+  },
+  {
+    "name": "digital_products",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "digital_products"
+  },
+  {
+    "name": "company_name",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "company_name"
+  },
+  {
+    "name": "created_on",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE",
+    "description": "created_on"
+  },
+  {
+    "name": "updated_at",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE",
+    "description": "updated_at"
+  },
+  {
+    "name": "account_id",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+    "description": "account_id"
+  }
 ]
 EOF
 
